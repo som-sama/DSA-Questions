b690504eca6a85786e03da065eb33cf592b5a702
Som <96942390+soumdatta81@users.noreply.github.com>
2022-10-19 20:34:42 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (94.66%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+       for(int i= digits.length-1; i>=0; i--){
+           if(digits[i]<9){
+               digits[i]++;
+               return digits; 
+           }
+           digits[i] = 0;
+       }
+        int[] Number = new int [digits.length+1];
+        Number [0] = 1;
+            return Number;
+    }
+}
