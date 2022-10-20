9c0d98847095e565f7e23d3ab6513b610398a629
Som <96942390+soumdatta81@users.noreply.github.com>
2022-10-20 08:55:07 +0530

    Time: 178 ms (53.70%), Space: 68 MB (74.77%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] prime = new boolean[n];
+        for(int  i=0; i<prime.length; i++){
+            prime[i] = true;
+        }
+        
+        for(int i=2; i*i< prime.length; i++){
+            if(prime[i]){
+                for(int j =i ; j*i<prime.length; j++){
+                    prime[i*j] = false;
+                }
+            }
+        }
+        
+        int primeCount = 0;
+        for(int i= 2; i < prime.length; i++){
+            if(prime[i]){
+                primeCount++;
+            }
+        }
+        return primeCount;
+        
+    }
+}
